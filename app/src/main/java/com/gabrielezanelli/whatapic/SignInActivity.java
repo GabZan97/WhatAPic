cfb4e2fcf486e2b435f5ae8b9c4d1babc6f18083
Gabriele Zanelli <gabrielezanelli@MacBook-Pro-di-Gabriele.local>
2017-05-15 10:58:14 +0200

    Created simple activities for login and gallery purposes

--- app/src/main/java/com/gabrielezanelli/whatapic/SignInActivity.java
+++ app/src/main/java/com/gabrielezanelli/whatapic/SignInActivity.java
@@ -2,6 +2,13 @@ package com.gabrielezanelli.whatapic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * Starting activity containing elements for obtaining user's access to instagram's account
+ */
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button buttonSignIn = (Button) findViewById(R.id.button_sign_in);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptInstagramAuthorization();
+            }
+        });
     }
+
+    private void attemptInstagramAuthorization() {
+
+    }
+
+
 }
